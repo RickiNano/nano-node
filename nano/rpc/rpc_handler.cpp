d6fbf0def92438497f5149495a49be721d170b7f
RickiNano <81099017+RickiNano@users.noreply.github.com>
2023-11-19 10:30:02 +0100

    Remove p tree from rpc_handler

--- nano/rpc/rpc_handler.cpp
+++ nano/rpc/rpc_handler.cpp
@@ -7,15 +7,14 @@
 #include <nano/lib/rpcconfig.hpp>
 #include <nano/rpc/rpc_handler.hpp>
 
-#include <boost/property_tree/json_parser.hpp>
-
 #include <unordered_set>
+
+#include <nlohmann/json.hpp>
 
 namespace
 {
 std::unordered_set<std::string> create_rpc_control_impls ();
 std::unordered_set<std::string> rpc_control_impl_set = create_rpc_control_impls ();
-std::string filter_request (boost::property_tree::ptree tree_a);
 }
 
 nano::rpc_handler::rpc_handler (nano::rpc_config const & rpc_config, std::string const & body_a, std::string const & request_id_a, std::function<void (std::string const &)> const & response_a, nano::rpc_handler_interface & rpc_handler_interface_a, nano::logger_mt & logger) :
@@ -54,20 +53,15 @@ void nano::rpc_handler::process_request (nano::rpc_handler_request_params const 
 		{
 			if (request_params.rpc_version == 1)
 			{
-				boost::property_tree::ptree request;
-				{
-					std::stringstream ss;
-					ss << body;
-					boost::property_tree::read_json (ss, request);
-				}
+				auto json_request = nlohmann::json::parse (body);
+				auto action = json_request["action"].get<std::string> ();
 
-				auto action = request.get<std::string> ("action");
 				if (rpc_config.rpc_logging.log_rpc)
 				{
 					// Creating same string via stringstream as using it directly is generating a TSAN warning
 					std::stringstream ss;
 					ss << request_id;
-					logger.always_log (ss.str (), " ", filter_request (request));
+					logger.always_log (ss.str ());
 				}
 
 				// Check if this is a RPC command which requires RPC enabled control
@@ -79,39 +73,6 @@ void nano::rpc_handler::process_request (nano::rpc_handler_request_params const 
 				{
 					json_error_response (response, rpc_control_disabled_ec.message ());
 					error = true;
-				}
-				else
-				{
-					// Special case with stats, type -> objects
-					if (action == "stats" && !rpc_config.enable_control)
-					{
-						if (request.get<std::string> ("type") == "objects")
-						{
-							json_error_response (response, rpc_control_disabled_ec.message ());
-							error = true;
-						}
-					}
-					else if (action == "process")
-					{
-						auto force = request.get_optional<bool> ("force").value_or (false);
-						if (force && !rpc_config.enable_control)
-						{
-							json_error_response (response, rpc_control_disabled_ec.message ());
-							error = true;
-						}
-					}
-					// Add random id to RPC send via IPC if not included
-					else if (action == "send" && request.find ("id") == request.not_found ())
-					{
-						nano::uint128_union random_id;
-						nano::random_pool::generate_block (random_id.bytes.data (), random_id.bytes.size ());
-						std::string random_id_text;
-						random_id.encode_hex (random_id_text);
-						request.put ("id", random_id_text);
-						std::stringstream ostream;
-						boost::property_tree::write_json (ostream, request);
-						body = ostream.str ();
-					}
 				}
 
 				if (!error)
@@ -188,39 +149,5 @@ std::unordered_set<std::string> create_rpc_control_impls ()
 	return set;
 }
 
-std::string filter_request (boost::property_tree::ptree tree_a)
-{
-	// Replace password
-	boost::optional<std::string> password_text (tree_a.get_optional<std::string> ("password"));
-	if (password_text.is_initialized ())
-	{
-		tree_a.put ("password", "password");
-	}
-	// Save first 2 symbols of wallet, key, seed
-	boost::optional<std::string> wallet_text (tree_a.get_optional<std::string> ("wallet"));
-	if (wallet_text.is_initialized () && wallet_text.get ().length () > 2)
-	{
-		tree_a.put ("wallet", wallet_text.get ().replace (wallet_text.get ().begin () + 2, wallet_text.get ().end (), wallet_text.get ().length () - 2, 'X'));
-	}
-	boost::optional<std::string> key_text (tree_a.get_optional<std::string> ("key"));
-	if (key_text.is_initialized () && key_text.get ().length () > 2)
-	{
-		tree_a.put ("key", key_text.get ().replace (key_text.get ().begin () + 2, key_text.get ().end (), key_text.get ().length () - 2, 'X'));
-	}
-	boost::optional<std::string> seed_text (tree_a.get_optional<std::string> ("seed"));
-	if (seed_text.is_initialized () && seed_text.get ().length () > 2)
-	{
-		tree_a.put ("seed", seed_text.get ().replace (seed_text.get ().begin () + 2, seed_text.get ().end (), seed_text.get ().length () - 2, 'X'));
-	}
-	std::string result;
-	std::stringstream stream;
-	boost::property_tree::write_json (stream, tree_a, false);
-	result = stream.str ();
-	// removing std::endl
-	if (result.length () > 1)
-	{
-		result.pop_back ();
-	}
-	return result;
-}
+
 }
