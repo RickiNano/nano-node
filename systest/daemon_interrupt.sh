f600e473cc5722fc10d9efde559adc3f3608670a
RickiNano <81099017+RickiNano@users.noreply.github.com>
2024-04-17 19:50:51 +0200

    kill888

--- systest/daemon_interrupt.sh
+++ systest/daemon_interrupt.sh
@@ -11,30 +11,8 @@ NODE_PID=$!
 sleep 10
 
 # Send an interrupt signal to the node process
-if [[ "$OSTYPE" == "msys" ]]; then
-    # For Windows, use PowerShell to stop the process
-    echo "Node stop"
-    powershell -Command "& {
-        $processName = 'nano_node.exe'
 
-        # Get all processes with the specified name
-        $processes = Get-Process -Name $processName -ErrorAction SilentlyContinue
-
-        if ($processes) {
-            # Send the CTRL+C event to each process with the specified name
-            foreach ($process in $processes) {
-                $process | ForEach-Object {
-                    $_.CloseMainWindow()
-                }
-            }
-        } else {
-            Write-Host 'No processes with the specified name were found.'
-        }
-    }"
-else
-    # For Unix-based systems, use kill
-    kill -SIGINT $NODE_PID
-fi
+	kill -INT 888 $NODE_PID
 
 # Check if the process has stopped using a timeout to avoid infinite waiting
 if wait $NODE_PID; then
