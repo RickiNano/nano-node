511c4c7a39ef1ca9b1ac2d09d821b795d7588458
RickiNano <81099017+RickiNano@users.noreply.github.com>
2024-04-18 14:32:17 +0200

    More debug

--- systest/daemon_interrupt.sh
+++ systest/daemon_interrupt.sh
@@ -8,9 +8,12 @@ $NANO_NODE_EXE --daemon --network dev --data_path $DATADIR &
 NODE_PID=$!
 
 # Allow some time for the node to start up completely
-sleep 10
+echo "Start wait"
+sleep 20
+echo "End wait"
 
 # Send an interrupt signal to the node process
+echo "Sending kill signal"
 kill -SIGINT $NODE_PID
 
 # Check if the process has stopped using a timeout to avoid infinite waiting
