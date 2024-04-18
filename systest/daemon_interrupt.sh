6d795383c9d439279ab4e9fe0947fa250b1d0f2a
RickiNano <81099017+RickiNano@users.noreply.github.com>
2024-04-18 11:00:34 +0200

    SigInt with debug

--- systest/daemon_interrupt.sh
+++ systest/daemon_interrupt.sh
@@ -11,14 +11,9 @@ NODE_PID=$!
 sleep 10
 
 # Send an interrupt signal to the node process
-if [[ "$OSTYPE" == "msys" ]]; then
-    # For Windows, use PowerShell to stop the process
-    
-	taskkill //IM "nano_node.exe"
-else
-    # For Unix-based systems, use kill
+
     kill -SIGINT $NODE_PID
-fi
+
 # Check if the process has stopped using a timeout to avoid infinite waiting
 if wait $NODE_PID; then
     echo "Node stopped successfully"
