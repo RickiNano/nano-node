2e1916a35578b5d8ebeac6fca1513f1d2d08dbbc
RickiNano <81099017+RickiNano@users.noreply.github.com>
2024-05-09 00:34:41 +0200

    new test

--- nano/node/election.hpp
+++ nano/node/election.hpp
@@ -64,12 +64,12 @@ private:
 	std::function<void (std::shared_ptr<nano::block> const &)> confirmation_action;
 	std::function<void (nano::account const &)> live_vote_action;
 
-private:
+public:
 	// Election time to live
-	std::chrono::milliseconds election_max_duration = std::chrono::milliseconds (10 * 60 * 1000);
-	std::chrono::milliseconds normal_election_max_idle = std::chrono::milliseconds (30 * 1000);
-	std::chrono::milliseconds hinted_election_max_idle = std::chrono::milliseconds (10 * 1000);
-	std::chrono::milliseconds optimistic_election_max_idle = std::chrono::milliseconds (10 * 1000);
+	const std::chrono::milliseconds election_max_duration{ std::chrono::milliseconds (10 * 60 * 1000) };
+	const std::chrono::milliseconds normal_election_max_idle{ std::chrono::milliseconds (30 * 1000) };
+	const std::chrono::milliseconds hinted_election_max_idle{ std::chrono::milliseconds (10 * 1000) };
+	const std::chrono::milliseconds optimistic_election_max_idle{ std::chrono::milliseconds (10 * 1000) };
 
 private: // State management
 	static unsigned constexpr passive_duration_factor = 5;
