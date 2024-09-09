a35f0c82ef95e04c0e5724363b810469ebe06e60
RickiNano <81099017+RickiNano@users.noreply.github.com>
2024-09-09 19:32:37 +0200

    Auto detect database backend

--- nano/node/make_store.hpp
+++ nano/node/make_store.hpp
@@ -1,18 +1,14 @@
 #pragma once
 
-#include <nano/lib/diagnosticsconfig.hpp>
-#include <nano/lib/lmdbconfig.hpp>
 #include <nano/lib/logging.hpp>
-#include <nano/lib/rocksdbconfig.hpp>
+#include <nano/node/nodeconfig.hpp>
 
 #include <chrono>
 
 namespace nano
 {
 class ledger_constants;
-class lmdb_config;
-class rocksdb_config;
-class txn_tracking_config;
+class node_config;
 }
 
 namespace nano::store
@@ -22,5 +18,5 @@ class component;
 
 namespace nano
 {
-std::unique_ptr<nano::store::component> make_store (nano::logger &, std::filesystem::path const & path, nano::ledger_constants & constants, bool open_read_only = false, bool add_db_postfix = true, nano::rocksdb_config const & rocksdb_config = nano::rocksdb_config{}, nano::txn_tracking_config const & txn_tracking_config_a = nano::txn_tracking_config{}, std::chrono::milliseconds block_processor_batch_max_time_a = std::chrono::milliseconds (5000), nano::lmdb_config const & lmdb_config_a = nano::lmdb_config{}, bool backup_before_upgrade = false, bool force_use_write_queue = false);
+std::unique_ptr<nano::store::component> make_store (nano::logger & logger, std::filesystem::path const & path, nano::ledger_constants & constants, bool read_only = false, bool add_db_postfix = true, nano::node_config const & node_config = nano::node_config{}, std::chrono::milliseconds block_processor_batch_max_time_a = std::chrono::milliseconds (5000), bool backup_before_upgrade = false, bool force_use_write_queue = false);
 }
