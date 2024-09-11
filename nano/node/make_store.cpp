1aaefb0a9d087c580313a2c6ea12dc9b8101809f
RickiNano <81099017+RickiNano@users.noreply.github.com>
2024-09-11 20:24:43 +0200

    Fixed unit tests

--- nano/node/make_store.cpp
+++ nano/node/make_store.cpp
@@ -5,6 +5,24 @@
 
 std::unique_ptr<nano::store::component> nano::make_store (nano::logger & logger, std::filesystem::path const & path, nano::ledger_constants & constants, bool read_only, bool add_db_postfix, nano::node_config const & node_config, std::chrono::milliseconds block_processor_batch_max_time_a, bool backup_before_upgrade, bool force_use_write_queue)
 {
+	const char * unit_testing_backend = std::getenv ("BACKEND");
+	if (unit_testing_backend != nullptr)
+	{
+		// make_store was called from unit test. Set database backend accordingly
+		std::cout << "DB backend: " << unit_testing_backend << std::endl;
+		if (std::strcmp (unit_testing_backend, "rocksdb"))
+		{
+			std::cout << "DB : " << unit_testing_backend << std::endl;
+			return std::make_unique<nano::store::rocksdb::component> (logger, add_db_postfix ? path / "rocksdb" : path, constants, node_config.rocksdb_config, read_only, force_use_write_queue);
+		}
+		if (std::strcmp (unit_testing_backend, "lmdb"))
+		{
+			std::cout << "DB : " << unit_testing_backend << std::endl;
+			return std::make_unique<nano::store::lmdb::component> (logger, add_db_postfix ? path / "data.ldb" : path, constants, node_config.diagnostics_config.txn_tracking, block_processor_batch_max_time_a, node_config.lmdb_config, backup_before_upgrade);
+		}
+		debug_assert (false);
+	}
+
 	if (node_config.database_backend == nano::database_backend::lmdb)
 	{
 		return std::make_unique<nano::store::lmdb::component> (logger, add_db_postfix ? path / "data.ldb" : path, constants, node_config.diagnostics_config.txn_tracking, block_processor_batch_max_time_a, node_config.lmdb_config, backup_before_upgrade);
