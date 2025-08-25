62c9682589839daef9ddd52b94d50fab6153abea
Gustav Schauwecker <service@simpago.com>
2025-08-25 13:24:50 +0200

    Make thread pool nullable

--- node/src/utils/thread_pool.rs
+++ node/src/utils/thread_pool.rs
@@ -1,3 +1,5 @@
+use std::{collections::VecDeque, sync::Mutex};
+
 pub struct ThreadPool {
     strategy: Strategy,
 }
@@ -16,7 +18,7 @@ impl ThreadPool {
 
     pub fn new_null() -> Self {
         Self {
-            strategy: Strategy::Nulled,
+            strategy: Strategy::Nulled(ThreadPoolStub::default()),
         }
     }
 
@@ -26,21 +28,31 @@ impl ThreadPool {
     {
         match &self.strategy {
             Strategy::Real(pool) => pool.execute(callback),
-            Strategy::Nulled => {}
+            Strategy::Nulled(pool) => pool.enqueue(callback),
         }
     }
 
     pub fn join(&self) {
         match &self.strategy {
             Strategy::Real(pool) => pool.join(),
-            Strategy::Nulled => {}
+            Strategy::Nulled(_) => {}
         }
     }
 
     pub fn queued_count(&self) -> usize {
         match &self.strategy {
             Strategy::Real(pool) => pool.queued_count(),
-            Strategy::Nulled => 0,
+            Strategy::Nulled(pool) => pool.queued_count(),
+        }
+    }
+
+    /// Only available if the pool is nulled! Run the enqueued tasks.
+    pub fn simulate(&self) {
+        match &self.strategy {
+            Strategy::Real(_) => {
+                panic!("Simulating executions is only possible with a nulled thread pool!")
+            }
+            Strategy::Nulled(pool) => pool.execute_queued_tasks(),
         }
     }
 }
@@ -53,7 +65,38 @@ impl Drop for ThreadPool {
 
 enum Strategy {
     Real(threadpool::ThreadPool),
-    Nulled,
+    Nulled(ThreadPoolStub),
+}
+
+struct ThreadPoolStub {
+    queue: Mutex<VecDeque<Box<dyn FnOnce() + Send>>>,
+}
+
+impl Default for ThreadPoolStub {
+    fn default() -> Self {
+        Self {
+            queue: Mutex::new(VecDeque::new()),
+        }
+    }
+}
+
+impl ThreadPoolStub {
+    fn enqueue(&self, task: impl FnOnce() + Send + 'static) {
+        self.queue.lock().unwrap().push_back(Box::new(task));
+    }
+
+    fn queued_count(&self) -> usize {
+        self.queue.lock().unwrap().len()
+    }
+
+    fn execute_queued_tasks(&self) {
+        loop {
+            let Some(task) = self.queue.lock().unwrap().pop_front() else {
+                break;
+            };
+            task();
+        }
+    }
 }
 
 #[cfg(test)]
@@ -80,12 +123,26 @@ mod tests {
     }
 
     #[test]
+    #[should_panic(expected = "Simulating executions is only possible with a nulled thread pool!")]
+    fn panic_when_simulate_called_on_real_pool() {
+        let pool = ThreadPool::new(1, "test thread".to_string());
+        pool.simulate();
+    }
+
+    #[test]
     fn can_be_nulled() {
         let pool = ThreadPool::new_null();
         let called = Arc::new(AtomicBool::new(false));
         let called2 = called.clone();
+
         pool.execute(move || called2.store(true, Ordering::SeqCst));
-        assert_eq!(pool.queued_count(), 0);
+
+        assert_eq!(pool.queued_count(), 1);
         assert_false!(called.load(Ordering::SeqCst));
+
+        pool.simulate();
+
+        assert_eq!(pool.queued_count(), 0);
+        assert!(called.load(Ordering::SeqCst));
     }
 }
