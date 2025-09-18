e16e56a91eaf5c9199389569a506115cc9edba2b
Gustav Schauwecker <service@simpago.com>
2025-09-18 12:59:14 +0200

    Reduce parallel bootstrap queries

--- node/src/bootstrap/state/bootstrap_logic.rs
+++ node/src/bootstrap/state/bootstrap_logic.rs
@@ -65,7 +65,7 @@ impl BootstrapLogic {
                 && self
                     .running_queries
                     .count_by_account(account, QuerySource::Priority)
-                    < 4
+                    < 2
         });
 
         if next.account.is_zero() {
